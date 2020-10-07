e583c5dbf5b65093647e61aa0ede4ca8ac69261c
Biersack <fallen-angels.fedotova@yandex.ru>
2020-10-07 23:18:40 +0300

    Initial commit

---
+++ src/main/java/ru/sfedu/constrcaclconsol/Constants.java
@@ -0,0 +1,7 @@
+package ru.sfedu.constrcaclconsol;
+
+public class Constants {
+    static public final int TEST_CONST=33;
+    static public final String ENV_CONST="source";
+
+}
---
+++ src/main/java/ru/sfedu/constrcaclconsol/ConstrCalcClient.java
@@ -0,0 +1,52 @@
+package ru.sfedu.constrcaclconsol;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.sfedu.constrcaclconsol.utils.ConfigurationUtil;
+
+import java.io.IOException;
+
+public class ConstrCalcClient {
+
+    private static Logger log = LogManager.getLogger(ConstrCalcClient.class);
+
+
+
+    public  ConstrCalcClient() {
+
+        log.debug("LogClient : starting application.........");
+    }
+
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        new ConstrCalcClient() ;
+        logBasicSystemInfo();
+        log.info(Constants.TEST_CONST);
+        System.out.print(ConfigurationUtil.getConfigurationEntry(Constants.ENV_CONST));
+    }
+
+
+    private static void logBasicSystemInfo() {
+        log.info("Launching the application...");
+        log.info(
+                "Operating System: " + System.getProperty("os.name") + " "
+                        + System.getProperty("os.version")
+        );
+        log.info("JRE: " + System.getProperty("java.version"));
+        log.info("Java Launched From: " + System.getProperty("java.home"));
+        log.info("Class Path: " + System.getProperty("java.class.path"));
+        log.info("Library Path: " + System.getProperty("java.library.path"));
+        log.info("User Home Directory: " + System.getProperty("user.home"));
+        log.info("User Working Directory: " + System.getProperty("user.dir"));
+        log.info("Test INFO logging.");
+    }
+
+
+
+
+
+}
+
