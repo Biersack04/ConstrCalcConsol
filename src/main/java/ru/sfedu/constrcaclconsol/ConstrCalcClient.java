f48b056aa7f5af2c938b580eefc7f88e3568fe36
Biersack <fallen-angels.fedotova@yandex.ru>
2020-10-19 16:49:09 +0300

    Second commit

--- src/main/java/ru/sfedu/constrcaclconsol/ConstrCalcClient.java
+++ src/main/java/ru/sfedu/constrcaclconsol/ConstrCalcClient.java
@@ -3,50 +3,39 @@ package ru.sfedu.constrcaclconsol;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.sfedu.constrcaclconsol.utils.ConfigurationUtil;
-
 import java.io.IOException;
 
 public class ConstrCalcClient {
+    private static int testConst;
 
     private static Logger log = LogManager.getLogger(ConstrCalcClient.class);
-
-
-
     public  ConstrCalcClient() {
 
         log.debug("LogClient : starting application.........");
     }
 
-
-
-
-
     public static void main(String[] args) throws IOException {
         new ConstrCalcClient() ;
         logBasicSystemInfo();
         log.info(Constants.TEST_CONST);
-        System.out.print(ConfigurationUtil.getConfigurationEntry(Constants.ENV_CONST));
+        log.info(ConfigurationUtil.getConfigurationEntry(Constants.ENV_CONST));
+        testConst=Constants.TEST_CONST;
+        log.info(String.format("The value of constant: %d%n",testConst));
+        log.error("Error log");
     }
 
 
-    private static void logBasicSystemInfo() {
+    public static void logBasicSystemInfo() {
         log.info("Launching the application...");
-        log.info(
-                "Operating System: " + System.getProperty("os.name") + " "
-                        + System.getProperty("os.version")
-        );
-        log.info("JRE: " + System.getProperty("java.version"));
-        log.info("Java Launched From: " + System.getProperty("java.home"));
+        log.info("Operating System: " + System.getProperty("os.name") + " " + System.getProperty("os.version"));
+        log.trace("JRE: " + System.getProperty("java.version"));
+        log.debug("Java Launched From: " + System.getProperty("java.home"));
         log.info("Class Path: " + System.getProperty("java.class.path"));
         log.info("Library Path: " + System.getProperty("java.library.path"));
-        log.info("User Home Directory: " + System.getProperty("user.home"));
+        log.warn("User Home Directory: " + System.getProperty("user.home"));
         log.info("User Working Directory: " + System.getProperty("user.dir"));
         log.info("Test INFO logging.");
+
     }
-
-
-
-
-
 }
 
---
+++ src/test/java/ru/sfedu/constrcaclconsol/ConstrCalcClientTest.java
@@ -0,0 +1,17 @@
+package ru.sfedu.constrcaclconsol;
+
+import org.junit.jupiter.api.Test;
+
+
+class ConstrCalcClientTest {
+
+    @Test
+    public void main() {
+        System.out.println("main");
+        System.out.println(Constants.TEST_CONST);
+        System.out.println("Good_test");
+        ConstrCalcClient client  = new ConstrCalcClient();
+        client.logBasicSystemInfo();
+        //fail("bad");
+    }
+}
